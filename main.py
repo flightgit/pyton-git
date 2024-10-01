28ffbd92130b10316a71bc70d95610eef2b12127
Ron <ron@Rons-MacBook-Pro.local>
2024-10-01 12:47:55 +0300

    Message

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from datetime import datetime
-current_year = datetime.now().year
-print(current_year)
-
 class Student:
     def __init__(self, name22, year22, id22):
         self.name = name22
@@ -9,5 +5,3 @@ class Student:
         self.id = id22
 
 s1 = Student("Adam",2000, "01234")
-
-print(s1.name, s1.year, s1.id)
